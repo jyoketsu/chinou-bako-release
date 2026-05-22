8cd216c62c0ac665a2b7f46c21e8f5f9762da45e
popoteto <jyoketsu@gmail.com>
2026-05-22 17:23:25 +0800

    chore: 删除无用的API路由示例文件

--- src/app/api/api-routes-example/route.ts
+++
@@ -1,9 +0,0 @@
-export async function POST(request: Request) {
-  const { field } = await request.json();
-
-  console.log("---field---", field);
-
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
